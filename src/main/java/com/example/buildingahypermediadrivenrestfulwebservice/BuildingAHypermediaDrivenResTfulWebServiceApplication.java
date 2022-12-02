e759786356101397440b1d3f14022d7a3f666c6a
Shizuka Tanigawa <chocokkuma@gmail.com>
2022-12-02 23:03:41 +0100

    first commit

---
+++ src/main/java/com/example/buildingahypermediadrivenrestfulwebservice/BuildingAHypermediaDrivenResTfulWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.buildingahypermediadrivenrestfulwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildingAHypermediaDrivenResTfulWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuildingAHypermediaDrivenResTfulWebServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/buildingahypermediadrivenrestfulwebservice/BuildingAHypermediaDrivenResTfulWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.buildingahypermediadrivenrestfulwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildingAHypermediaDrivenResTfulWebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
